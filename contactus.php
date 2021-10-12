ce5321bfdb03842f9247faf17a9042a10944285e
hamza javed <51706015+HamzaJavedxix@users.noreply.github.com>
2021-10-12 22:32:23 +0530

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,345 @@
+<?php
+
+    $error = ""; $successMessage = "";
+
+    if ($_POST) {
+        
+        if (!$_POST["email"]) {
+            
+            $error .= "An email address is required.<br>";
+            
+        }
+        
+        if (!$_POST["content"]) {
+            
+            $error .= "The content field is required.<br>";
+            
+        }
+        
+        if (!$_POST["subject"]) {
+            
+            $error .= "The subject is required.<br>";
+            
+        }
+        
+        if ($_POST['email'] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
+            
+            $error .= "The email address is invalid.<br>";
+            
+        }
+        
+        if ($error != "") {
+            
+            $error = '<div class="alert alert-danger" role="alert"><p>There were error(s) in your form:</p>' . $error . '</div>';
+            
+        } else {
+            
+            $emailTo = "support@notesjugaad.com";
+            
+            $subject = $_POST['subject'];
+            
+            $content = $_POST['content'];
+            
+            $headers = "From: ".$_POST['email'];
+            
+            if (mail($emailTo, $subject, $content, $headers)) {
+                
+                $successMessage = '<div class="alert alert-success" role="alert">Your message was sent, we\'ll get back to you ASAP!</div>';
+                
+                
+            } else {
+                
+                $error = '<div class="alert alert-danger" role="alert"><p><strong>Your message couldn\'t be sent - please try again later</div>';
+                
+                
+            }
+            
+        }
+        
+        
+        
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Contact Us</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="favicon.png" rel="icon">
+  <link href="apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="assets/vendor/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="container-fluid">
+
+      <div id="logo" class="pull-left">
+        <h1><a href="#intro" class="scrollto">NotesJugaad</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="#intro"><img src="3.png" alt=""></a>-->
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+          <li class="menu-active"><a href="index.php">Home</a></li>
+          <li><a href="about.html">About Us</a></li>
+          <li class="menu-has-children"><a href="">Notes</a>
+            <ul>
+              <li><a href="ipu.html">IPU</a></li>
+              <li><a href="hbtu.html">HBTU</a></li>
+              <li><a href="du.html">DU</a></li>
+		<li><a href="gate.html">GATE</a></li>
+            </ul>
+          </li>
+          <li><a href="#intro">Contact</a></li>
+        </ul>
+      </nav><!-- #nav-menu-container -->
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Intro Section ======= -->
+  <section id="intro">
+    <div class="intro-container">
+      <div id="introCarousel" class="carousel  slide carousel-fade" data-ride="carousel">
+
+        <ol class="carousel-indicators"></ol>
+
+        <div class="carousel-inner" role="listbox">
+
+          <div class="carousel-item active">
+            <div class="carousel-background"><img src="img/contact.jpeg" alt=""></div>
+            <div class="carousel-container">
+              <div class="carousel-content">
+                <h2>NotesJugaad</h2>
+               <h3><p>Solution to all your Class Problems at a single Platform</p></h3>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </section><!-- End Intro Section -->
+
+  
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="section-bg wow fadeInUp">
+      <div class="container">
+
+        <div class="section-header">
+          <h3>Contact Us</h3>
+          <p>Need to communicate with your seniors?, Get suggestions from Qualified Professors? Whatever it is let us know through this feedback form. We will try to improve our website according to your suggestion. Feel free to reach us.</p>
+<p>Want to be a Student Partner or a Business Partner with us, then also drop us a mail.</p>
+        </div>
+
+        <div class="row contact-info">
+
+          <div class="col-md-4">
+            <div class="contact-address">
+              <i class="ion-ios-location-outline"></i>
+              <h3>Address</h3>
+              <address></address>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-phone">
+              <i class="ion-ios-telephone-outline"></i>
+              <h3>Phone Number</h3>
+              <p><a href="tel:"></a></p>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="contact-email">
+              <i class="ion-ios-email-outline"></i>
+              <h3>Email</h3>
+              <p><a href="mailto:notesjugaad1@gmail.com">support@notesjugaad.com</a></p>
+            </div>
+          </div>
+
+        </div>
+
+      <div class="container">
+      
+    <h1>Get in touch!</h1>
+      
+      <div id="error"><? echo $error.$successMessage; ?></div>
+      
+      <form method="post">
+  <fieldset class="form-group">
+    <label for="email">Email address</label>
+    <input type="email" class="form-control" id="email" name="email" placeholder="Enter email">
+    <small class="text-muted">We'll never share your email with anyone else.</small>
+  </fieldset>
+  <fieldset class="form-group">
+    <label for="subject">Subject</label>
+    <input type="text" class="form-control" id="subject" name="subject" >
+  </fieldset>
+  <fieldset class="form-group">
+    <label for="exampleTextarea">What would you like to ask us?</label>
+    <textarea class="form-control" id="content" name="content" rows="3"></textarea>
+  </fieldset>
+  <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+</form>
+          
+        </div>
+
+    <!-- jQuery first, then Bootstrap JS. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+          
+          
+    <script type="text/javascript">
+          
+          $("form").submit(function(e) {
+              
+              var error = "";
+              
+              if ($("#email").val() == "") {
+                  
+                  error += "The email field is required.<br>"
+                  
+              }
+              
+              if ($("#subject").val() == "") {
+                  
+                  error += "The subject field is required.<br>"
+                  
+              }
+              
+              if ($("#content").val() == "") {
+                  
+                  error += "The content field is required.<br>"
+                  
+              }
+              
+              if (error != "") {
+                  
+                 $("#error").html('<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form:</strong></p>' + error + '</div>');
+                  
+                  return false;
+                  
+              } else {
+                  
+                  return true;
+                  
+              }
+          })
+          
+    </script>
+          
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+ <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h4>NotesJugaad</h4>
+            <p>Here at NotesJugaad, you’ll find in-depth, insanely useful notes covering the things you truly need to know in order to be a successful student.<br>
+I hope you find this site helpful to you! Feel free to reach out at any time.</p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="ion-ios-arrow-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="about.html">About us</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="tos.html">Terms of service</a></li>
+              <li><i class="ion-ios-arrow-right"></i> <a href="pop.html">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              <strong>Email:</strong> support@notesjugaad.com<br>
+            </p>
+
+            <div class="social-links">
+              <a href="https://twitter.com/notesjugaad1" class="twitter"><i class="fa fa-twitter"></i></a>
+              <a href="https://www.facebook.com/notes.jugaad.94" class="facebook"><i class="fa fa-facebook"></i></a>
+              <a href="https://www.instagram.com/notesjugaad1/" class="instagram"><i class="fa fa-instagram"></i></a>
+              <a href="mailto:notesjugaad1@gmail.com" class="google-plus"><i class="fa fa-google-plus"></i></a>
+              <a href="https://www.linkedin.com/in/notes-jugaad-5929b91a8" class="linkedin"><i class="fa fa-linkedin"></i></a>
+            </div>
+
+          </div>
+          <div class="col-lg-3 col-md-6 footer-newsletter">
+            <h4>Our Newsletter</h4>
+            <p>If you want to Subscribe for our Newsletters, then kindly enter your Email address below</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>NOTESJUGAAD</strong>. Some Rights Reserved
+      </div>
+  </footer><!-- End Footer -->
+ 
+
+
+  <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+  <!-- Uncomment below i you want to use a preloader -->
+  <!-- <div id="preloader"></div> -->
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/wow/wow.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/superfish/superfish.min.js"></script>
+  <script src="assets/vendor/hoverIntent/hoverIntent.js"></script>
+  <script src="assets/vendor/jquery-touchswipe/jquery.touchSwipe.min.js"></script>
+
+  <!-- Template Main- JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
